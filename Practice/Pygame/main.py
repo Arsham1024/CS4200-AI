fee46ed68e3d1c0f9b477a0ae73bc2d80e14d565
Arsham Mehrani <Arsham512@gmail.com>
2022-03-29 11:09:51 -0700

    basic movement implemented needs work

--- Practice/Pygame/main.py
+++ Practice/Pygame/main.py
@@ -14,13 +14,14 @@ pygame.display.set_icon(icon)
 # Player vars
 player_img = pygame.image.load('letter-a.png')
 # X and Y axis to position : Y in upside down
-player_x = 370
-player_y = 480
+playerX = 370
+playerY = 480
+playerX_change = 0
 
 # PLAYER
-def player():
+def player(x,y):
     # draw the player icon. blit = draw
-    screen.blit(player_img, (player_x,player_y))
+    screen.blit(player_img, (x,y))
 
 
 running = True
@@ -28,14 +29,28 @@ running = True
 while running:
     # in RGB values
     screen.fill((229,255,204))
-    # draw the player
-    player()
 
     for event in pygame.event.get():
         # if user quits it
         if event.type == pygame.QUIT:
             running = False
+
+    # if key stroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            print("Keystroke registered")
+            if event.key == pygame.K_LEFT:
+                playerX_change -= 0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change += 0.1
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                pass
+            if event.key == pygame.K_RIGHT:
+                pass
     
-    
+    # draw the player
+    playerX += playerX_change
+    player(playerX,playerY)
     # necessary line to update, add everytime
     pygame.display.update()
